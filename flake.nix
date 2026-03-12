d6b804c9e654972902fd006e05608fa1c91276d3
Nicolas Chan <nicolas@nicolaschan.com>
2026-03-11 20:08:07 -0700

    Add vps cluster

--- flake.nix
+++ flake.nix
@@ -87,7 +87,7 @@
       });
 
       devShells = eachSystem (_system: pkgs: {
-        default = pkgs.mkShell { packages = gleamBuildInputs pkgs; };
+        default = pkgs.mkShell { packages = (gleamBuildInputs pkgs) ++ (with pkgs; [ fluxcd k9s ]); };
       });
     };
 }
